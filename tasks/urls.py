9513b5d0568a4c62f8f8d76276df1175702cb32d
João Nogueira <joaonogueira@fisica.ufc.br>
2021-08-30 10:08:12 -0300

    passing variables to views

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('helloworld/', views.helloworld),
+    path('', views.tasklist, name="task-list"),
+    path('yourname/<str:name>', views.yourname, name="your-name"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 
 def helloworld(request):
     return HttpResponse('Hello World!')
+
+
+def tasklist(request):
+    return render(request, 'tasks/list.html')
+
+
+def yourname(request, name):
+    return render(request, 'tasks/yourname.html', {'name': name})
