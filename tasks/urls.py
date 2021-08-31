4de572e1df6d7e0fa67f2f381e026999839ceca5
João Nogueira <joaonogueira@fisica.ufc.br>
2021-08-31 16:40:02 -0300

    Create Task Form

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('helloworld/', views.helloworld),
-    path('', views.tasklist, name="task-list"),
-    path('task/<int:id>', views.taskview, name="task-view"),
-    path('yourname/<str:name>', views.yourname, name="your-name"),
+    path('', views.index, name="task-index"),
+    path('task/', views.index, name="task-index"),
+    path('task/<int:id>', views.show, name="task-show"),
+    path('task/create', views.create, name="task-create"),
+    path('task/store', views.store, name="task-store"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,21 +1,28 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import TaskForm
 
 from .models import Task
 
-def tasklist(request):
-    tasks = Task.objects.all()
-    return render(request, 'tasks/list.html', {'tasks': tasks})
+def index(request):
+    tasks = Task.objects.all().order_by('-created_at')
+    return render(request, 'tasks/index.html', {'tasks': tasks})
 
 
-def taskview(request, id):
+def show(request, id):
     task = get_object_or_404(Task, pk=id)
-    return render(request, 'tasks/task.html', {'task': task})
+    return render(request, 'tasks/show.html', {'task': task})
 
 
-def helloworld(request):
-    return HttpResponse('Hello World!')
+def create(request):
+    form = TaskForm()
+    return render(request, 'tasks/create.html', {'form': form})
 
 
-def yourname(request, name):
-    return render(request, 'tasks/yourname.html', {'name': name})
+def store(request):
+    form = TaskForm(request.POST)
+    if form.is_valid():
+        task = form.save(commit=False)
+        task.status = 'doing'
+        task.save()
+        return redirect('/')
