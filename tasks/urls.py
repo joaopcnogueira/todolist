f31b5cc2785e6d4b18f0fe6b9bba2ff1fccd7635
João Nogueira <joaonogueira@fisica.ufc.br>
2021-08-31 10:24:46 -0300

    Read task done

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('helloworld/', views.helloworld),
     path('', views.tasklist, name="task-list"),
+    path('task/<int:id>', views.taskview, name="task-view"),
     path('yourname/<str:name>', views.yourname, name="your-name"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,20 @@
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Task
+
+def tasklist(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/list.html', {'tasks': tasks})
+
+
+def taskview(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'task': task})
 
 
 def helloworld(request):
     return HttpResponse('Hello World!')
-
-
-def tasklist(request):
-    return render(request, 'tasks/list.html')
 
 
 def yourname(request, name):
